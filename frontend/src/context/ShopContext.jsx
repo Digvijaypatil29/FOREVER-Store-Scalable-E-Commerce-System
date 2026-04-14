b1602b62af45c5e10c2a06c094b5a9af4eb57f02
Digvijay Patil <digvijaypatil001100@gmail.com>
2026-04-14 22:39:33 +0530

    Fix backendUrl string formatting in ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = "₹";
     const delivery_fee = 10;
-    const backendUrl = https://forever-store-scalable-e-commerce-system.onrender.com;
+    const backendUrl = "https://forever-store-scalable-e-commerce-system.onrender.com";
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState([]);
